f144412b1fd364040356ded9fa82585c7d46d206
denisfeo <dfeoktistov523@icloud.com>
2021-02-22 20:32:04 +0300

    Window size and place on screen added.

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ from MainWindow.MainWindow import MainWindow
 
 
 class App:
+    WINDOW_WIDTH = 1000
+    WINDOW_HEIGHT = 800
+
     def __init__(self):
         self.master = tkinter.Tk()
+
+        SCREEN_WIDTH = self.master.winfo_screenwidth()
+        SCREEN_HEIGHT = self.master.winfo_screenheight()
+
+        WINDOW_X = (SCREEN_WIDTH - App.WINDOW_WIDTH) // 2
+        WINDOW_Y = (SCREEN_HEIGHT - App.WINDOW_HEIGHT) // 2
+
+        self.master.geometry(f"{App.WINDOW_WIDTH}x{App.WINDOW_HEIGHT}+{WINDOW_X}+{WINDOW_Y}")
         self.main_window = MainWindow(self)
 
     def show(self):
