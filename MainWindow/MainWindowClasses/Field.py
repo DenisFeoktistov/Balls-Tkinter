d021aaeadd6bdd0f8677a49fb9cd93bf5b3a43f2
elkcl <28754537+elkcl@users.noreply.github.com>
2021-02-22 22:10:58 +0300

    Added canvas and border.

--- MainWindow/MainWindowClasses/Field.py
+++ MainWindow/MainWindowClasses/Field.py
@@ -14,8 +14,13 @@ class Field:
         self.relheight = relheight
 
         self.init_title()
-        #self.init_canvas()
+        self.init_canvas()
 
     def init_title(self):
         self.title = tkinter.Label(self.window.app.master, text='Field', justify=tkinter.CENTER, bg=self.window.COLOR, fg='#000000', font='sans 20')
         self.title.place(relx=self.relx, rely=self.rely, relwidth=self.relwidth, relheight=0.05*self.relheight)
+
+    def init_canvas(self):
+        self.canvas = tkinter.Canvas(self.window.app.master, bg=self.window.COLOR, width=self.relwidth*self.window.WINDOW_WIDTH, height=0.95*self.relheight*self.window.WINDOW_HEIGHT, highlightthickness=7)
+        self.canvas.place(relx=self.relx, rely=self.rely+0.05*self.relheight, relwidth=self.relwidth, relheight=0.95*self.relheight)
+        #self.canvas.create_oval((100, 100), (300, 300), fill='green')
