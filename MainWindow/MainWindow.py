1ffe08769783c62965436f1ab6c9dcf37d5c048b
denisfeo <dfeoktistov523@icloud.com>
2021-02-22 20:46:51 +0300

    Cosmetic changes.

--- MainWindow/MainWindow.py
+++ MainWindow/MainWindow.py
@@ -9,13 +9,22 @@ class MainWindow:
     def __init__(self, app):
         self.app = app
 
-        SCREEN_WIDTH = self.app.master.winfo_screenwidth()
-        SCREEN_HEIGHT = self.app.master.winfo_screenheight()
-
-        window_x = (SCREEN_WIDTH - MainWindow.WINDOW_WIDTH) // 2
-        window_y = (SCREEN_HEIGHT - MainWindow.WINDOW_HEIGHT) // 2
-
-        self.app.master.geometry(f"{MainWindow.WINDOW_WIDTH}x{MainWindow.WINDOW_HEIGHT}+{window_x}+{window_y}")
+        self.resize()
+        self.adding_background()
+        self.adding_title()
 
         self.settings = Settings(self)
         self.field = Field(self)
+
+    def adding_background(self):
+        self.app.master["bg"] = "#505050"
+
+    def adding_title(self):
+        self.app.master.title("Balls")
+
+    def resize(self):
+        SCREEN_WIDTH = self.app.master.winfo_screenwidth()
+        SCREEN_HEIGHT = self.app.master.winfo_screenheight()
+        window_x = (SCREEN_WIDTH - MainWindow.WINDOW_WIDTH) // 2
+        window_y = (SCREEN_HEIGHT - MainWindow.WINDOW_HEIGHT) // 2
+        self.app.master.geometry(f"{MainWindow.WINDOW_WIDTH}x{MainWindow.WINDOW_HEIGHT}+{window_x}+{window_y}")
